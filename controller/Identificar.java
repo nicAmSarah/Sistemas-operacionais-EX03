82c84914c261da1b34e92c290f0a5db9cebef34b
nicAmSarah <nicaciosarah085@gmail.com>
2026-02-22 22:23:15 -0300

    Identificando valores do vetor - 2.0

--- controller/Identificar.java
+++ controller/Identificar.java
@@ -11,9 +11,9 @@ public class Identificar {
 	public void identificarVet(int[]vet) {
 		for(int numero : vet) {
 			if(numero % 2 != 0) {
-				JOptionPane.showMessageDialog(null, "o valor" + numero + " é impar");
-			}else if(numero % 10 != 10){
-				JOptionPane.showMessageDialog(null, numero + " é par e múltiplo de 10");
+				JOptionPane.showMessageDialog(null, "o valor " + numero + " é impar");
+			}else if(numero % 10 == 0){
+				JOptionPane.showMessageDialog(null, numero + " é par e múltiplo de 10");				
 			}
 			
 
